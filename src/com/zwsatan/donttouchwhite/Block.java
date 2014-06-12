a4837acea90f45d9746b301feae90c4efe896e45
zwsatan <zwsatan420@163.com>
2014-06-12 14:39:41 +0800

    微调代码

--- src/com/zwsatan/donttouchwhite/Block.java
+++ src/com/zwsatan/donttouchwhite/Block.java
@@ -12,27 +12,6 @@ import android.graphics.Paint.Align;
 import android.graphics.Paint.Style;
 
 public class Block {
-	// 方块的位置信息
-	private float x;
-	private float y;
-	private float width;
-	private float height;
-
-	// 方块的颜色信息
-	private int color;
-
-	// 方块的速度信息
-	private float speed;
-	
-	// 记录黑块被点击动画的缩放的效果
-	private float scaleForBlackTouched;
-	// 记录白块被点击时动画的红白红白变效果
-	private int animForWhiteTouched;
-	// 记录黑块漏点的情况下的动画效果，仅在经典模式下
-	private int animForBlackNotTouched = 0;
-	
-	// 记录是否为起始方块，就是有两字开始
-	private boolean isStartBlock;
 
 	public Block(float x, float y, float width, float height, int color, float speed) {
 		this.x = x;
@@ -53,26 +32,26 @@ public class Block {
 	public float getY() {
 		return y;
 	}
-	
+
 	public void setY(float y) {
 		this.y = y;
 	}
-	
+
 	public int getColor() {
 		return color;
 	}
-	
+
 	public boolean contains(float touchX, float touchY) {
 		boolean inX = x <= touchX && touchX <= x + width;
 		boolean inY = y <= touchY && touchY <= y + height;
 		return inX && inY;
 	}
-	
+
 	public void moveDown(float speedAcc) {
 		y += speed;
 		speed += speedAcc;
 	}
-	
+
 	public void draw(Canvas canvas, Paint paint) {
 		paint.setColor(color);
 		paint.setStyle(Style.FILL);
@@ -84,9 +63,9 @@ public class Block {
 			paint.setStyle(Style.STROKE);
 			canvas.drawRect(x,  y, x + width, y + height, paint);
 		}
-		
+
 		drawBlackTouchAnim(scaleForBlackTouched, canvas, paint);
-		
+
 		// 绘制开始方块
 		if (isStartBlock) {
 			paint.setColor(Color.WHITE);
@@ -95,11 +74,11 @@ public class Block {
 			canvas.drawText("开始", x + width / 2, y + height / 2, paint);
 		}
 	}
-	
+
 	public void startBlackAnim() {
 		ValueAnimator animator = ValueAnimator.ofFloat(0.7f, 1.0f);
 		animator.addUpdateListener(new AnimatorUpdateListener() {
-			
+
 			@Override
 			public void onAnimationUpdate(ValueAnimator animator) {
 				// 获取当前灰色方块的缩放值
@@ -111,15 +90,15 @@ public class Block {
 			}
 		});
 		animator.setDuration(200).start();
-		
+
 		SoundEngine.getSoundEngine().playPianoSound();
 	}
-	
+
 	public void startBlackNotTouchedAnim() {
 		animForBlackNotTouched = 0;
 		final Timer timer = new Timer();
 		timer.schedule(new TimerTask() {
-			
+
 			@Override
 			public void run() {
 				if (color == Color.BLACK) {
@@ -127,19 +106,19 @@ public class Block {
 				} else if (color == Color.GRAY) {
 					color = Color.BLACK;
 				}
-				
+
 				if (6 == animForBlackNotTouched++) {
 					timer.cancel();
 				}
 			}
 		}, 0, 50);
 	}
-	
+
 	public void startWhiteAnim() {
 		animForWhiteTouched = 0;
 		final Timer timer = new Timer();
 		timer.schedule(new TimerTask() {
-			
+
 			@Override
 			public void run() {
 				if (color == Color.RED) {
@@ -147,24 +126,45 @@ public class Block {
 				} else if (color == Color.WHITE) {
 					color = Color.RED;
 				}
-				
+
 				if (6 == animForWhiteTouched++) {
 					timer.cancel();
 				}
 			}
 		}, 0, 50);
 	}
-	
+
 	public void drawBlackTouchAnim(float scale, Canvas canvas, Paint paint) {
 		float newWidth = width * scale;
 		float newHeight = height * scale;
 		float newX = x + (width - newWidth) / 2;
 		float newY = y + (height - newHeight) / 2;
-		
+
 		paint = new Paint();
 		paint.setColor(Color.GRAY);
 		paint.setStyle(Style.FILL);
 		canvas.drawRect(newX, newY, newX + newWidth, newY + newHeight, paint);
 	}
-	
+
+	// 方块的位置信息
+	private float x;
+	private float y;
+	private float width;
+	private float height;
+
+	// 方块的颜色信息
+	private int color;
+
+	// 方块的速度信息
+	private float speed;
+
+	// 记录黑块被点击动画的缩放的效果
+	private float scaleForBlackTouched;
+	// 记录白块被点击时动画的红白红白变效果
+	private int animForWhiteTouched;
+	// 记录黑块漏点的情况下的动画效果，仅在经典模式下
+	private int animForBlackNotTouched = 0;
+
+	// 记录是否为起始方块，就是有两字开始
+	private boolean isStartBlock;
 }
